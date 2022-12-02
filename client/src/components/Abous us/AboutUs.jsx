a18e2849537575036ca45273f13cb72430f83277
Ervan Joviangit config --global --replace-all  user.name Ervan <ervanjovian@gmail.com>
2022-12-02 17:50:11 +0700

    Add about us on home page
    Udah buat about us di home page tapi masih dummy gambarrnya

---
+++ client/src/components/Abous us/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './AboutUs.style.css'
+import Dummy from '../../assets/dummyBackground-herosection.jpg'
+
+const AboutUs = () => {
+  return (
+    <div className="aboutus-container">
+      <div className='aboutus-content'>
+        <div className='description'>
+          <h1>ABOUT US</h1>
+          <br/>
+          <p>One Trip is a trip planner app for users to determine the destination 
+            of the tourist attractions they will go to. This app should help users, 
+            to collect information about the places they want to go based on 
+            their location choices, and plan their trip.
+          </p>
+          <br/>
+          <br/>
+          <button>READ MORE</button>
+        </div>
+        <div className="image-description">
+          <img src={Dummy} alt="Dummy"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUs
