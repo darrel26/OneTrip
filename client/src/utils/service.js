a6acb9ae4fdcd006922d02b9bbcf3fac6968f2fd
Noid <dionisiusgunadi@outlook.com>
2022-12-03 18:03:39 +0700

    add function handler for sign up

---
+++ client/src/utils/service.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const baseURL = 'http://localhost:3001/api';
+
+const userSignUp = async (newUser) => {
+    const request = axios.post(`${baseURL}/users/register`, newUser);
+    const res = await request;
+    console.log(res.data);
+    return res.data;
+}
+
+export { userSignUp };
