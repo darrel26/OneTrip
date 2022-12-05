97be36f4db774ae3d938bf018dfbcd085885ef83
Ervan Joviangit config --global --replace-all  user.name Ervan <ervanjovian@gmail.com>
2022-12-05 12:17:50 +0700

    Create CTA-Section
    Buat cta-section, tapi backgroundnya itu gw ga pake css edittingnnya karna ga nemu caranya, jadi gambarnya biar rada gelap gw edit biasa terus gw import di css

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 import Navbar from '../components/HeroSection/Navbar'
 import Content from '../components/HeroSection/Content'
 import AboutUs from '../components/AboutUs/AboutUs'
-
+import CTASection from '../components/CTASection/CTASection'
 import './HomePage.style.css'
+
 
 const HomePage = () => {
   return (
@@ -15,6 +16,9 @@ const HomePage = () => {
         </div>
         <div className='aboutus-container-home'>
           <AboutUs/>
+        </div>
+        <div>
+          <CTASection/>
         </div>
     </div>
   )
