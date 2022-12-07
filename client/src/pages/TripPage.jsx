d0d45309894e23d84c99db75afc3758c3d811b0d
Ervan Joviangit config --global --replace-all  user.name Ervan <ervanjovian@gmail.com>
2022-12-07 21:44:31 +0700

    Update trip page
    Ganti beberapa module, ngerapihin code, sama nambah .env

--- client/src/pages/TripPage.jsx
+++ client/src/pages/TripPage.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import { LoadScript } from "@react-google-maps/api"
+import { useJsApiLoader } from "@react-google-maps/api"
 import './TripPage.style.css'
 import GoogleMap from '../components/GoogleMapSection/GoogleMapSection'
 import NavbarTrip from '../components/TripSection/NavbarTrip'
 
+const library = ['places']
 const TripPage = () => {
   const center = {
     lat: -3.745,
     lng: -38.523
   };
+
+  const {isLoaded, loadError} = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API,
+    libraries: library
+  })
+
   return (
     <div className='trippage-maincontainer'>
-      <LoadScript
-       googleMapsApiKey='AIzaSyBuDXGh0iay6VVMlb3X7Odap7W3mS8ZZiE'
-      >
-        <div className='trippage-container-left'>
-          <NavbarTrip/>
-        </div>
-        <div className='trippage-container-right'>
-          <GoogleMap mapCenter={center}/>
-        </div>
-      </LoadScript>
+      <div className='trippage-container-left'>
+        <NavbarTrip/>
+      </div>
+      <div className='trippage-container-right'>
+        {isLoaded? <GoogleMap mapCenter={center}/> : loadError}
+      </div>
     </div>
   )
 }
