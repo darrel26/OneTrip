2e1ba27b321b38fe91b161a66c23f1d733faf253
Ervan Joviangit config --global --replace-all  user.name Ervan <ervanjovian@gmail.com>
2022-12-09 15:27:59 +0700

    Add budget
    Add budget feature preview

--- client/src/DummyData.jsx
+++ client/src/DummyData.jsx
@@ -1,8 +1,8 @@
 export const placeVisit = 
     {
         tripName : 'Trip to San Fransisco',
-        dateStart: '11-12-2022',
-        dateEnd: ' 14-12-2022',
+        dateStart: '11/25',
+        dateEnd: ' 11/30',
         trip: [
                 {
                 objectId:"1",
@@ -15,6 +15,15 @@ export const placeVisit =
                 placeName : "Glenpark",
                 description: "Glen Park is a residential neighborhood in San Francisco, California, located south of Twin Peaks and adjacent to Glen Canyon Park.",
                 image: "https://s.winoper.com/winoper/Tour-00764171.jpeg"
+                },
+                {
+                objectId:"3",
+                placeName : "Bandung",
+                description: "Bandung, is a city in Indonesia and the capital of West Java province, one of the cities that often becomes a city for tourists because of its natural beauty.",
+                image: "https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/10/01/3219367321.jpg"
                 }
-              ]
+              ],
+        budgetData : {
+          targetBudget : 1000000,
+        }
     }
--- client/src/components/TripSection/EditTrip.jsx
+++ client/src/components/TripSection/EditTrip.jsx
@@ -4,10 +4,13 @@ import calendar from '../../assets/Calendar-icon-trip.svg'
 import './EditTrip.style.css'
 import { placeVisit } from '../../DummyData'
 import PlacePreview from './PlacePreview'
+import Budget from './Budget'
 
 const EditTrip = () => {
-
-  const [isOpen, setIsOpen] = useState(false);
+  // eslint-disable-next-line
+  const [dataTrip , setDataTrip] = useState(placeVisit);
+  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenBudget, setIsOpenBudget] = useState(true);
 
   return (
     <div className='edit-trip-container'>
@@ -19,12 +22,24 @@ const EditTrip = () => {
         <div className='accordion'>
             <div className='accordion-title' onClick={() => setIsOpen(!isOpen)}>
               <img className='toggle' src={chevron} alt="chevron icon" aria-expanded={isOpen}/>
-              <h1>Places to visit</h1>
+              <h1 className='title'>Places to visit</h1>
             </div>
             <div className='accordion-content' aria-expanded={!isOpen}>
-             {placeVisit.trip.map((item, index)=>(
+             {dataTrip.trip.map((item, index)=>(
                 <PlacePreview key={item.objectId} placeImg={item.image} index={index} placeDesc={item.description} placeName={item.placeName}/>
              ))}
+            </div>
+        </div>
+        <div className='accordion'>
+            <div className='accordion-title-budget'>
+              <div className='budget-title' onClick={() => setIsOpenBudget(!isOpenBudget)}>
+                <img className='toggle' src={chevron} alt="chevron icon" aria-expanded={isOpenBudget}/>
+                <h1 className='title'>Budgetting</h1>
+              </div>
+              <button>ADD EXPENSES</button>
+            </div>
+            <div className='accordion-content' aria-expanded={!isOpenBudget}>
+              <Budget value={dataTrip.budgetData.targetBudget}/>
             </div>
         </div>
     </div>
