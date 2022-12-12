557798f09dbf3e1fad8af7d8ad8a136a07480553
Noid <dionisiusgunadi@outlook.com>
2022-12-12 17:45:08 +0700

    integrate notification alert using sweetalert2

---
+++ client/src/utils/notificationAlert.js
@@ -0,0 +1,31 @@
+import Swal from "sweetalert2"
+
+const succesToast = (message) => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    return Toast.fire({
+      icon: 'success',
+      title: `${message}`
+    })
+}
+
+const errorAlert = (errorStatus, errorMessage) => {
+    return Swal.fire({
+        title: errorStatus,
+        text: errorMessage,
+        icon: 'error',
+        confirmButtonText: 'Retry'
+    })
+}
+
+export {succesToast, errorAlert};
