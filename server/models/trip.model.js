1b11352e2596c2c73ae8a6012e40abb729f5be78
Noid <dionisiusgunadi@outlook.com>
2022-12-14 19:44:40 +0700

    create trip router and model file

---
+++ server/models/trip.model.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    basedLocation: {
+        type: String,
+        required: true,
+    },
+    places: [
+        {
+            type: Object,
+        },
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    budget: {
+        type: Number,
+    },
+    expenses: [
+        {
+            category: String,
+            amount: Number,
+        },
+    ],
+});
+
+tripSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
