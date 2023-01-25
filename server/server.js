da5559a965c655ec56ecd80c2bd26a9074c0a524
Noid <dionisiusgunadi@outlook.com>
2023-01-25 15:02:56 +0700

    try to fix bug

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { PORT } = require('./utils/config');
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(process.env.npm_config_env === 'production' ? 443 : PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log(`Server running in ${process.env.npm_config_env} mode`);
 });
